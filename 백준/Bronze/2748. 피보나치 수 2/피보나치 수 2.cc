faab36a0be8b860a8100b61a9544677adc413a4b
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2022-08-06 14:54:52 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+   int n;
+
+   scanf("%d",&n);
+   long long arr[91] = {0,1};
+
+   for(int i=2;i<=n;i++){
+      arr[i] = arr[i-1] + arr[i-2];
+   }
+   
+   printf("%lld\n",arr[n]);
+
+   return 0;
+}
