cf8d0dbd3a42c9048bed8a7d4f6246d879af4945
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2022-08-10 22:43:44 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+   int n,k,p,i,j;
+   int check[1001]={0,};
+
+   scanf("%d %d",&n,&k);
+
+   p=k;
+   printf("<%d",p);
+   check[k]=1;
+
+   for(i=0;i<n-1;i++){
+      for(j=0;j<k;j++){
+         while(1){
+            p++;
+            if(p>n) p=1;
+            if(check[p]==0) break;
+         }
+      }
+      printf(", %d",p);
+      check[p]=1;
+   }
+   printf(">");
+
+   return 0;
+}
