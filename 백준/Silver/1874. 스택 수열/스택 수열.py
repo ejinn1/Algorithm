a76faaf33a4bcf3c5e00f406c137eb7cdda95cb5
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2023-06-09 20:34:39 +0900

    [Silver II] Title: 스택 수열, Time: 5044 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,25 @@
+n = int(input())
+stack = []
+op = []
+cnt = 1
+f = True
+
+for i in range(n):
+    num = int(input())
+    while cnt <= num:
+        stack.append(cnt)
+        op.append('+')
+        cnt += 1
+        
+    if stack[-1] == num:
+        stack.pop()
+        op.append('-')
+    else:
+        f = False
+        break
+
+if f == False:
+    print('NO')
+else:
+    for i in op:
+        print(i)
