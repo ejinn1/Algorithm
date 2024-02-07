94ca3faef60a4059d0559c51f9b54d4b6b279d14
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2024-02-07 17:37:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 154.46 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,18 @@
+function solution(participant, completion) {
+    var answer = '';
+    
+    participant = participant.sort()
+    completion = completion.sort()
+    
+    console.log(participant)
+    console.log(completion)
+    
+    for(let i=0 ; i<participant.length ; i++){
+        if(participant[i] !== completion[i]){
+            answer = participant[i]
+            break
+        }
+    }
+    
+    return answer;
+}
