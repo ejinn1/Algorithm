35754d1c00f38003cc21a1e827b1b67df364a3c9
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2024-04-13 15:49:45 +0900

    [level 1] Title: 덧칠하기, Time: 3.49 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,16 +1,13 @@
 function solution(n, m, section) {
     
-    let walls = Array.from({length: n}).map((_, i) => i+1)
-    
-    walls = walls.map((wall) => section.includes(wall) ? 0 : wall)
-    
-    let result = 0
-    Array.from({length: walls.length}).forEach((_, i) => {
-        if(walls[i] === 0){
-            result += 1
-            Array.from({length: m}).forEach((_, j) => walls[i+j] === 0 ? walls[i+j] += 1 : null)
+    var answer = 0;
+    var painted = 0;
+    for(var s of section) {
+        if(painted < s) {
+            answer++;
+            painted = s+m-1;
         }
-    })
-    
-    return result
+    }
+    return answer;
+
 }
