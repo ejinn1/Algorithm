025b3776ebfdd8038aa72c5efbf6710006b26a93
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2024-04-16 15:09:21 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 117.83 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,18 @@
+function solution(str) {
+    const tmp = []
+    
+    
+    const result = str.split('').map((s) => {
+        if(tmp.includes(s)){
+            tmp.push(s)
+            const r = tmp.indexOf(s)
+            tmp[r] = -1
+            return tmp.indexOf(s) - r
+        } else {
+            tmp.push(s)
+            return -1
+        }
+    })
+    
+    return result
+}
