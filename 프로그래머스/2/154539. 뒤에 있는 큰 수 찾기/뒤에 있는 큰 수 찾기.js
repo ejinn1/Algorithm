b25d7d0f449331add5902229a6a49edb4c7f8d66
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2024-05-11 13:42:49 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 16.90 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,17 @@
+function solution(numbers) {
+    const n = numbers.length;
+    const result = new Array(n).fill(-1);
+    const stack = [];
+
+    for (let i = n - 1; i >= 0; i--) {
+        while (stack.length > 0 && stack[stack.length - 1] <= numbers[i]) {
+            stack.pop();
+        }
+        if (stack.length > 0) {
+            result[i] = stack[stack.length - 1];
+        }
+        stack.push(numbers[i]);
+    }
+
+    return result;
+}
