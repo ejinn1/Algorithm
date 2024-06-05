587207797d72a9c23904279f7d5af6dabd3fc160
ejinn1 <101779861+ejinn1@users.noreply.github.com>
2024-06-05 16:26:39 +0900

    [level 1] Title: 공원 산책, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -3,41 +3,45 @@ function solution(park, routes) {
     const W = park[0].length
     let x = 0
     let y = 0
-    park.forEach((p, i) => {
-        let j = p.indexOf('S')
-        if(j !== -1){
-            x = i
-            y = j
-        }
-    })
-    // 각 방향에 따른 x, y 좌표 변화량을 정의합니다.
-    const d = {
-        'E': [0,1],
-        'W': [0,-1],
-        'S': [1,0],
-        'N': [-1,0]
-    }
-    // 주어진 명령들을 처리합니다.
-    routes.forEach((route) => {
-        const [op, n] = route.split(" ")
-        const [dx, dy] = d[op]
-        
-        let newX = x
-        let newY = y
-        
-        for(let i=0 ; i<n ; i++){
-            newX += dx
-            newY += dy
-            
-            if (newX < 0 || newX >= H || newY < 0 || newY >= W || park[newX][newY] === 'X') {
-                newX = x
-                newY = y
-                break;
+    
+    for(let i=0 ; i<W ; i++){
+        for(let j=0 ; j<H ; j++){
+            if(park[i][j] === 'S'){
+                x = j
+                y = i
             }
         }
-        x = newX
-        y = newY
-    })
+    }
     
-    return [x, y]
+    const direction = {
+        'N': [-1, 0],
+        'S': [1, 0],
+        'W': [0, -1],
+        'E': [0, 1]
+    }
+    
+    routes.forEach((route) => {
+        const [op, n] = route.split(' ')
+        let cx = x
+        let cy = y
+        let f = true
+        
+        for(let i=0 ; i<n ; i++){
+            const [dy, dx] = direction[op]
+            cx += dx
+            cy += dy
+            
+            if(cx < 0 || cx >= W || cy < 0 || cy >= H || park[cy][cx] === 'X'){
+                f = false
+                break
+            }
+        }
+            
+        if(f){
+            x = cx
+            y = cy
+        }
+    })
+
+    return [y, x]
 }
