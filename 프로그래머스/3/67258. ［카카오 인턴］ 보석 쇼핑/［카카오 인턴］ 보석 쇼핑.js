2a129cdb16e9fbbbecdc568e4239d5fff047a985
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-01-04 15:23:58 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 22.02 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,25 +1,27 @@
 function solution(gems) {
-    const gemKinds = new Set(gems).size
+    const gemKind = new Set(gems).size
     const gemCount = new Map()
     let [start, end] = [0, 0]
     let [minStart, minEnd] = [0, gems.length - 1]
     
-    while (end < gems.length) {
+    while(end < gems.length){
         gemCount.set(gems[end], (gemCount.get(gems[end]) || 0) + 1)
         end++
-
-        while (gemCount.size === gemKinds) {
-            if (end - start < minEnd - minStart + 1) {
+        
+        while(gemCount.size === gemKind){
+            if(end - start < minEnd - minStart + 1){
                 [minStart, minEnd] = [start, end - 1]
             }
-
+            
             gemCount.set(gems[start], gemCount.get(gems[start]) - 1)
-            if (gemCount.get(gems[start]) === 0) {
+            if(gemCount.get(gems[start]) === 0){
                 gemCount.delete(gems[start])
             }
             start++
         }
     }
-
+    
     return [minStart + 1, minEnd + 1]
+    
+    
 }
