34bfc6cb8a1648b18266ef1d6928bed97d507eb5
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-03-17 18:22:20 +0900

    [level 2] Title: 구명보트, Time: 33.67 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,20 +1,23 @@
 function solution(people, limit) {
-    people = people.sort((a, b) => a - b)
+    people.sort((a, b) => a - b)
+    let cnt = 0
+    let res = 0
     
-    let result = 0
     let i=0
     let j=people.length-1
+    
     while(i <= j){
         if(people[i] + people[j] <= limit){
-            result++
+            // people = people.slice(i+1, j)
             i++
             j--
+            cnt++
         } else {
-            result++
+            res++
             j--
         }
     }
     
-    return result
+    return cnt + res
     
 }
