f5a1f29af6b6d69219509871d9453fe5186f3d6e
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-03-23 16:32:11 +0900

    [level 3] Title: 숫자 게임, Time: 92.31 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/README.md
+++ 프로그래머스/3/12987. 숫자 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 숫자 게임 - 12987 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12987#) 
 
 ### 성능 요약
 
-메모리: 45.5 MB, 시간: 65.23 ms
+메모리: 43.8 MB, 시간: 92.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 04일 22:07:25
+2025년 03월 23일 16:32:08
 
 ### 문제 설명
 
--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.js
@@ -1,21 +1,24 @@
 function solution(A, B) {
     let result = 0
-
+    
     A.sort((a, b) => a - b)
     B.sort((a, b) => a - b)
-
+    
+    console.log(A)
+    console.log(B)
+    
     let i = 0
     let j = 0
-
-    while (i < A.length && j < B.length) {
-        if (B[j] > A[i]) {
-            result++
+    while(i < A.length && j < B.length){
+        if(B[j] > A[i]){
             i++
             j++
+            result++
         } else {
             j++
         }
     }
 
+    
     return result
 }
