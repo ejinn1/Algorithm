9a79081a7c7e106752923aa338bc85df12500733
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-08-13 16:38:43 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 512 ms, Memory: 59428 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,11 +1,16 @@
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         List<Integer>[] graph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
@@ -13,17 +18,18 @@ public class Main {
         }
 
         for (int i = 0; i < N - 1; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph[a].add(b);
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             graph[b].add(a);
+            graph[a].add(b);
         }
 
         int[] parent = new int[N + 1];
         boolean[] visited = new boolean[N + 1];
         visited[1] = true;
 
-        Queue<Integer> q = new LinkedList<>();
+        ArrayDeque<Integer> q = new ArrayDeque<>();
         q.add(1);
 
         while (!q.isEmpty()) {
@@ -37,8 +43,11 @@ public class Main {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for(int i=2 ; i<=N ; i++){
-            System.out.println(parent[i]);
+            sb.append(parent[i]).append('\n');
         }
+        System.out.println(sb);
+
     }
 }
