ca11a3247bb9f8a869752d5b04fafab9c487a93c
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-09-02 16:57:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 180 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,13 +1,11 @@
 
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
   static int N;
   static int[][] arr;
   static boolean[][] visited;
-  static int num = 0;
   static int[] dx = {0, 0, 1, -1};
   static int[] dy = {1, -1, 0, 0};
 
@@ -15,7 +13,6 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     N = sc.nextInt();
-
     arr = new int[N][N];
 
     for (int i = 0; i < N; i++) {
@@ -26,41 +23,25 @@ public class Main {
     }
 
     visited = new boolean[N][N];
-
+    List<Integer> sizes = new ArrayList<>();
+    
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         if (arr[i][j] == 1 && !visited[i][j]) {
-          num++;
-          dfs(i, j);
+          sizes.add(dfs(i, j));
         }
       }
     }
 
-    System.out.println(num);
-
-    int[] result = new int[num+1];
-
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (arr[i][j] != 0) {
-          result[arr[i][j]]++;
-        }
-      }
-    }
-
-    Arrays.sort(result);
-
-    for (int i = 1; i <= num; i++) {
-      System.out.println(result[i]);
-    }
-
-
+    Collections.sort(sizes);
+    System.out.println(sizes.size());
+    for(int s: sizes) System.out.println(s);
   }
 
-  static void dfs(int x, int y) {
+  static int dfs(int x, int y) {
     visited[x][y] = true;
-    arr[x][y] = num;
-
+    int cnt = 1;
+    
     for (int d = 0; d < 4; d++) {
       int cx = x + dx[d];
       int cy = y + dy[d];
@@ -69,9 +50,9 @@ public class Main {
       if(arr[cx][cy] == 0) continue;
       if(visited[cx][cy]) continue;
 
-      dfs(cx, cy);
+      cnt += dfs(cx, cy);
     }
+
+    return cnt;
   }
-
-
 }
