8123bc039ff1a5a939e1dfe7ca411f7cdfaaca58
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-10-31 17:34:22 +0900

    [level 3] Title: 네트워크, Time: 0.75 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,23 +1,39 @@
 function solution(n, computers) {
-    const visited = Array(n).fill(false)
-    let result = 0
+    const graph = Array.from({length: n+1}, () => [])
     
-    function dfs(i){
-        visited[i] = true
-        
+    for(let i=0 ; i<n ; i++){
         for(let j=0 ; j<n ; j++){
-            if(j !== i && computers[i][j] === 1 && !visited[j]){
-                dfs(j)
+            if(i !== j && computers[i][j] === 1){
+                graph[i+1].push(j+1)
             }
         }
     }
     
-    for(let i=0 ; i<n ; i++){
+    const visited = Array(n+1).fill(false)
+    let result = 0
+    
+    
+    
+    function dfs(node){
+        if(visited[node]) return
+        
+        visited[node] = true
+        
+        for(const next of graph[node]){
+            dfs(next)
+        }
+    }
+    
+    
+    for(let i=1 ; i<=n ; i++){
         if(!visited[i]){
             dfs(i)
             result++
         }
+        
     }
+    
+    
     
     return result
 }
