04a7661c1c79f21748d0f66f753f739f5e26e60f
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-11-11 18:02:21 +0900

    [Gold IV] Title: 단어 수학, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -9,29 +9,30 @@ public class Main {
 
     long[] weight = new long[26];
 
-    String[] words = new String[N];
     for (int i = 0; i < N; i++) {
-      words[i] = sc.nextLine();
+      char[] word = sc.nextLine()
+              .toCharArray();
       int mul = 1;
-      char[] sub = words[i].toCharArray();
-      for (int j = sub.length - 1; j >= 0; j--) {
-        weight[sub[j] - 'A'] += mul;
+      for (int j = word.length - 1; j >= 0; j--) {
+        weight[word[j] - 'A'] += mul;
         mul *= 10;
       }
     }
-    
+
     List<Long> w = new ArrayList<>();
-    for (long v : weight) if (v > 0) w.add(v);
-    w.sort(Comparator.reverseOrder());
-    int digit = 9;
-    int result = 0;
+    for (long v : weight) {
+      if(v > 0) w.add(v);
+    }
+    w.sort(Collections.reverseOrder());
+
+    long digit = 9;
+    long result = 0;
     for (long v : w) {
-      result += v * digit;
+      result += digit * v;
       digit--;
-      if (digit < 0) break;
+      if(digit < 0) break;
     }
 
     System.out.println(result);
-    
   }
 }
