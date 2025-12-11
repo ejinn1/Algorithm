a150643821c6307896cb81c171a99b3924a28913
유의진 <101779861+ejinn1@users.noreply.github.com>
2025-12-11 17:10:44 +0900

    [Gold V] Title: 감소하는 수, Time: 192 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+  static List<Long> list = new ArrayList<>();
+
+  public static void main(String[] args) throws Exception{
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+
+    for (int i = 0; i <= 9; i++) {
+      dfs(i, i);
+    }
+
+    Collections.sort(list);
+
+    if (N >= list.size()) {
+      System.out.println(-1);
+    } else {
+      System.out.println(list.get(N));
+    }
+  }
+
+  static void dfs(long num, int lastDigit) {
+    list.add(num);
+
+    for (int next = 0; next < lastDigit; next++) {
+      long nextNum = num * 10 + next;
+      dfs(nextNum, next);
+    }
+  }
+}
