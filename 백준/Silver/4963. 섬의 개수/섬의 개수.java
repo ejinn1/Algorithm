efecc06db4ff35427e254d4c35c202ecff5d3536
유의진 <101779861+ejinn1@users.noreply.github.com>
2026-01-19 18:18:59 +0900

    [Silver II] Title: 섬의 개수, Time: 372 ms, Memory: 36916 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+  static int w, h;
+  static int[][] arr;
+  static boolean[][] visited;
+  static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+  static int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    while (true) {
+      w = sc.nextInt();
+      h = sc.nextInt();
+      if(w == 0 && h == 0) break;
+
+      arr = new int[h][w];
+      visited = new boolean[h][w];
+
+      for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+          arr[i][j] = sc.nextInt();
+        }
+      }
+
+      int cnt = 0;
+      for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+          if (arr[i][j] == 1 && !visited[i][j]) {
+            dfs(i, j);
+            cnt++;
+          }
+        }
+      }
+
+      System.out.println(cnt);
+    }
+  }
+
+  static void dfs(int x, int y) {
+    visited[x][y] = true;
+
+    for (int d = 0; d < 8; d++) {
+      int nx = x + dx[d];
+      int ny = y + dy[d];
+
+      if(nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+      if(visited[nx][ny]) continue;
+      if(arr[nx][ny] == 0) continue;
+
+      dfs(nx, ny);
+    }
+  }
+}
