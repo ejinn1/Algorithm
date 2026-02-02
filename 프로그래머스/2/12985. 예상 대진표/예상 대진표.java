c709b5decac9b8e7e5f102b0c4ba4e95c415d538
유의진 <101779861+ejinn1@users.noreply.github.com>
2026-02-02 11:45:05 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.Math;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {   
+        
+        int round = 1;
+        
+        while((a+1) / 2 != (b+1) / 2){
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+
+        return round;
+    }
+}
