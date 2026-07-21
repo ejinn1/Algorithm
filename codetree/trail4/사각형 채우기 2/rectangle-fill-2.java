3a5969e2b062ebde2d00e82b0dd710c69cca0293
유의진 <101779861+ejinn1@users.noreply.github.com>
2026-07-21 13:16:19 +0900

    [Passed] 사각형 채우기 2 (142ms, 10MB)

---
+++ codetree/trail4/사각형 채우기 2/rectangle-fill-2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    static int MOD = 10_007;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2 ; i<=N ; i++){
+            dp[i] = (dp[i-2] * 2 + dp[i-1]) % MOD;
+        }
+
+        System.out.print(dp[N]);
+    }
+}
